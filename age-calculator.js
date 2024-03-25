2c7c3b60ac79ca08a8ac2adc0ba23c24a4a7514b
syed-moulana7 <syedmoulana89@gmail.com>
2024-03-25 23:25:02 +0530

    age-calcuulator project for codealpha completed

---
+++ age-calculator.js
@@ -0,0 +1,19 @@
+const inputbtn = document.querySelector(".age-input-dob");
+const calculatebtn = document.querySelector(".cal-btn");
+const ageresult = document.querySelector(".age-result");
+
+calculatebtn.addEventListener("click", () => {
+    if (inputbtn.value === "") {
+        alert("Please enter your date of birth.");
+    } else {
+        const dob = new Date(inputbtn.value);
+        const dob_year = dob.getFullYear();
+
+        const now = new Date();
+        const now_year = now.getFullYear();
+        const age = now_year - dob_year;
+
+        
+        ageresult.innerHTML = `Your age is ${age}`;
+    }
+});
